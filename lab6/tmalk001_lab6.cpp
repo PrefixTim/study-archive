5af1c13f15875fd0b016b0836b8f634b84503567
Timofei Malko (OverSouls) <developer.of.gift@gmail.com>
2022-11-01 19:34:12 -0700

    UPDATE lab6

--- lab6/tmalk001_lab6.cpp
+++ lab6/tmalk001_lab6.cpp
@@ -23,7 +23,7 @@ typedef struct Task {
 #define NitrogenPump    5
 #define dispenser       6
 
-#define NUM_TASK 4
+#define NUM_TASK 1
 Task tasks[NUM_TASK];
 
 unsigned char pwms[3][4] = {
@@ -35,21 +35,21 @@ int PWM_Tick(int state) {//single state Task
     static unsigned char j = 3;
     static unsigned char waite_time = 0;
     static unsigned char cycles = 0;
-
     static unsigned char whater_level = 0;
     static unsigned char ice_level = 0;
     static unsigned char i = 0;
+    Serial.println(j);
     if(i % 5 == 0)whater_level = analogRead(WaterLevel) < 200;
     if (!(i++)) ice_level = analogRead(IceLevel) < 200;
     i %= 20;
-
+    Serial.println(whater_level);
+    Serial.println(ice_level);
+    digitalWrite(WaterLevelLed, whater_level && !ice_level);
+    digitalWrite(IceLevelLed, !whater_level && ice_level);
     if (whater_level || ice_level) {
-        if (whater_level && !ice_level) digitalWrite(WaterLevelLed, HIGH);
-        if (!whater_level && ice_level) digitalWrite(IceLevelLed, HIGH);
+        digitalWrite(pwms[j][0], LOW);
         return state;
-    } 
-    digitalWrite(WaterLevelLed, LOW);
-    digitalWrite(IceLevelLed, LOW);
+    }
     if (waite_time == 0) {
         digitalWrite(pwms[j][0], LOW);
         j = (j + 1) % 4;
@@ -59,16 +59,18 @@ int PWM_Tick(int state) {//single state Task
     digitalWrite(pwms[j][0], cycles++<pwms[j][2]);
     waite_time -= (cycles % 10) == 0;
     cycles %= pwms[j][1];
+    return state;
 }
 
 
 void setup() {
     for (char i = 2; i < 7; i++) pinMode(i, OUTPUT);
     tasks[0] = { .state = 0, .period = 10, .lastRun = 0, .TickFct = &PWM_Tick };
-    Serial.begin(9600);
+    Serial.begin(115200);
 }
 
 void loop() {
+    Serial.println("loop");
     for (int i = 0; i < NUM_TASK; i++) {
         if (millis() - tasks[i].lastRun >= tasks[i].period) {
             tasks[i].state = tasks[i].TickFct(tasks[i].state);
