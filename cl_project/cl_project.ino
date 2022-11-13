1b295d8b560fb9dfebcd6c601e8df91497aa2dd7
Timofei Malko (OverSouls) <developer.of.gift@gmail.com>
2022-11-13 02:58:49 -0800

    Update final project time

--- cl_project/cl_project.ino
+++ cl_project/cl_project.ino
@@ -2,122 +2,44 @@
 
 #include "Print.h"
 
-#define DS1307_I2C_ADDRESS 0x68
-
-#define MON 1
-#define TUE 2
-#define WED 3
-#define THU 4
-#define FRI 5
-#define SAT 6
-#define SUN 7
-
-class DS1307 {
-   private:
-    uint8_t decToBcd(uint8_t val);
-    uint8_t bcdToDec(uint8_t val);
-
-   public:
-    void begin();
-    // void begin(uint16_t SDA ,uint16_t SCL);
-    void startClock(void);
-    void stopClock(void);
-    void setTime(void);
-    void getTime(void);
-    void fillByHMS(uint8_t _hour, uint8_t _minute, uint8_t _second);
-    void fillByYMD(uint16_t _year, uint8_t _month, uint8_t _day);
-    void fillDayOfWeek(uint8_t _dow);
+const char* week_days[8] = {"", "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+struct TimeBcd {
     uint8_t second;
     uint8_t minute;
     uint8_t hour;
-    uint8_t dayOfWeek;  // day of week, 1 = Monday
+    uint8_t dayOfWeek;
     uint8_t dayOfMonth;
     uint8_t month;
-    uint16_t year;
+    uint8_t year;
 };
 
-uint8_t DS1307::decToBcd(uint8_t val) {
-    return ((val / 10 * 16) + (val % 10));
-}
+union TimeUnion {
+    TimeBcd time;
+    uint8_t arr[7];
+};
 
-// Convert binary coded decimal to normal decimal numbers
-uint8_t DS1307::bcdToDec(uint8_t val) {
-    return ((val / 16 * 10) + (val % 16));
-}
+struct DS1307 {
+    TimeUnion t;
+    void begin() {
+        Wire.begin();
+    }
 
-void DS1307::begin() {
-    Wire.begin();
-}
+    void setTime(TimeBcd _time) {
+        Wire.beginTransmission(0x68);
+        Wire.write((uint8_t)0x00);
+        for (auto i : t.arr) Wire.write(i);
+        Wire.endTransmission();
+        t.time = _time;
+    }
 
-/*Function: The clock timing will start */
-void DS1307::startClock(void) {  // set the ClockHalt bit low to start the rtc
-    Wire.beginTransmission(DS1307_I2C_ADDRESS);
-    Wire.write((uint8_t)0x00);  // Register 0x00 holds the oscillator start/stop bit
-    Wire.endTransmission();
-    Wire.requestFrom(DS1307_I2C_ADDRESS, 1);
-    second = Wire.read() & 0x7f;  // save actual seconds and AND sec with bit 7 (sart/stop bit) = clock started
-    Wire.beginTransmission(DS1307_I2C_ADDRESS);
-    Wire.write((uint8_t)0x00);
-    Wire.write((uint8_t)second);  // write seconds back and start the clock
-    Wire.endTransmission();
-}
-/*Function: The clock timing will stop */
-void DS1307::stopClock(void) {  // set the ClockHalt bit high to stop the rtc
-    Wire.beginTransmission(DS1307_I2C_ADDRESS);
-    Wire.write((uint8_t)0x00);  // Register 0x00 holds the oscillator start/stop bit
-    Wire.endTransmission();
-    Wire.requestFrom(DS1307_I2C_ADDRESS, 1);
-    second = Wire.read() | 0x80;  // save actual seconds and OR sec with bit 7 (sart/stop bit) = clock stopped
-    Wire.beginTransmission(DS1307_I2C_ADDRESS);
-    Wire.write((uint8_t)0x00);
-    Wire.write((uint8_t)second);  // write seconds back and stop the clock
-    Wire.endTransmission();
-}
-/****************************************************************/
-/*Function: Read time and date from RTC */
-void DS1307::getTime() {
-    // Reset the register pointer
-    Wire.beginTransmission(DS1307_I2C_ADDRESS);
-    Wire.write((uint8_t)0x00);
-    Wire.endTransmission();
-    Wire.requestFrom(DS1307_I2C_ADDRESS, 7);
-    // A few of these need masks because certain bits are control bits
-    second = bcdToDec(Wire.read() & 0x7f);
-    minute = bcdToDec(Wire.read());
-    hour = bcdToDec(Wire.read() & 0x3f);  // Need to change this if 12 hour am/pm
-    dayOfWeek = bcdToDec(Wire.read());
-    dayOfMonth = bcdToDec(Wire.read());
-    month = bcdToDec(Wire.read());
-    year = bcdToDec(Wire.read());
-}
-/*******************************************************************/
-/*Frunction: Write the time that includes the date to the RTC chip */
-void DS1307::setTime() {
-    Wire.beginTransmission(DS1307_I2C_ADDRESS);
-    Wire.write((uint8_t)0x00);
-    Wire.write(decToBcd(second));  // 0 to bit 7 starts the clock
-    Wire.write(decToBcd(minute));
-    Wire.write(decToBcd(hour));  // If you want 12 hour am/pm you need to set bit 6
-    Wire.write(decToBcd(dayOfWeek));
-    Wire.write(decToBcd(dayOfMonth));
-    Wire.write(decToBcd(month));
-    Wire.write(decToBcd(year));
-    Wire.endTransmission();
-}
-void DS1307::fillByHMS(uint8_t _hour, uint8_t _minute, uint8_t _second) {
-    // assign variables
-    hour = _hour;
-    minute = _minute;
-    second = _second;
-}
-void DS1307::fillByYMD(uint16_t _year, uint8_t _month, uint8_t _day) {
-    year = _year - 2000;
-    month = _month;
-    dayOfMonth = _day;
-}
-void DS1307::fillDayOfWeek(uint8_t _dow) {
-    dayOfWeek = _dow;
-}
+    void getTime() {
+        Wire.beginTransmission(0x68);
+        Wire.write((uint8_t)0x00);
+        Wire.endTransmission();
+        Wire.requestFrom(0x68, 7);
+        for (auto& i : t.arr) i = Wire.read();
+    }
+};
 
 class LCD1602REG : public Print {
    public:
@@ -184,12 +106,6 @@ void setup() {
     Serial.begin(115200);
     lcd.begin();
     clock.begin();
-
-    // clock.fillByYMD(2022,11,9);//Jan 19,2013
-    // clock.fillByHMS(23,53,20);//15:28 30"
-    // clock.fillDayOfWeek(WED);//Saturday
-    // clock.setTime();//write time to the RTC chip
-
 }
 
 void loop() {
@@ -197,49 +113,27 @@ void loop() {
     delay(1000);
 }
 
-/*Function: Display time on the serial monitor*/
 void printTime() {
     lcd.home();
     clock.getTime();
-    lcd.print(clock.hour, DEC);
+    lcd.print(clock.t.time.hour >> 4 & 0xF);
+    lcd.print(clock.t.time.hour & 0xF);
     lcd.print(":");
-    lcd.print(clock.minute, DEC);
+    lcd.print(clock.t.time.minute >> 4);
+    lcd.print(clock.t.time.minute & 0xF);
     lcd.print(":");
-    lcd.print(clock.second, DEC);
+    lcd.print(clock.t.time.second >> 4 & 0xF);
+    lcd.print(clock.t.time.second & 0xF);
     lcd.print("  ");
-    lcd.print(clock.month, DEC);
-    lcd.print("/");
-    lcd.print(clock.dayOfMonth, DEC);
-    lcd.print("/");
-    lcd.print(clock.year + 2000, DEC);
+    lcd.print(week_days[clock.t.time.dayOfWeek]);
     lcd.setCursor(0, 1);
-    lcd.print(clock.dayOfMonth);
-    lcd.print("*");
-    switch (clock.dayOfWeek)  // Friendly printout the weekday
-    {
-        case MON:
-            lcd.print("MON");
-            break;
-        case TUE:
-            lcd.print("TUE");
-            break;
-        case WED:
-            lcd.print("WED");
-            break;
-        case THU:
-            lcd.print("THU");
-            break;
-        case FRI:
-            lcd.print("FRI");
-            break;
-        case SAT:
-            lcd.print("SAT");
-            break;
-        case SUN:
-            lcd.print("SUN");
-            break;
-        default:
-            lcd.print("FUCK");
-            break;
-    }
+    lcd.print(clock.t.time.month >> 4);
+    lcd.print(clock.t.time.month & 0xF);
+    lcd.print("/");
+    lcd.print(clock.t.time.dayOfMonth >> 4);
+    lcd.print(clock.t.time.dayOfMonth & 0xF);
+    lcd.print("/");
+    lcd.print(20);
+    lcd.print(clock.t.time.year >> 4);
+    lcd.print(clock.t.time.year & 0xF);
 }
