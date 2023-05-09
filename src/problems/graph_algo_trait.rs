ffa47bfcf3c13510ad36a42c941ee9300a3a5bd3
Timofey Malko <developer.of.gift@gmail.com>
2023-05-09 13:51:52 -0700

    Revert "FIX"
    This reverts commit b247fc824fffb0f0cc901c2faf4ffdc3dc9bed1a.

--- src/problems/graph_algo_trait.rs
+++ src/problems/graph_algo_trait.rs
@@ -14,36 +14,16 @@ pub fn graph_search<'a>(problem: &mut impl Problem<'a>, print: bool) -> Result<(
 
     let mut expanded = 0;
     let mut max_queue = 0;
-    let mut best_goal = Some(problem.get_node(1).clone());
-    best_goal = None;
     loop {
         if frontier.is_empty() {
-            if let Some(goal) = &best_goal {
-                println!("Goal!!!");
-                let node = best_goal.unwrap();
-                return Ok((
-                    SolutionStats::new(expanded, max_queue, node.get_depth()),
-                    (&node).into(),
-                ));
-            }
             return Err(());
         }
 
-        max_queue = max_queue.max(frontier.len() as i64);
-        
-        let node = problem.get_node(frontier.pop().unwrap().get_id()).clone();
-        if let Some(goal) = &best_goal {
-            if goal.get_cost() <= node.get_cost() {
-                println!("Goal!!!");
-                let node = best_goal.unwrap();
-                return Ok((
-                    SolutionStats::new(expanded, max_queue, node.get_depth()),
-                    (&node).into(),
-                ));
-            }
-        }
-        
         expanded += 1;
+        max_queue = max_queue.max(frontier.len() as i64);
+
+        let node = problem.get_node(frontier.pop().unwrap().get_id()).clone();
+
         problem.print_expand(&node);
 
         if problem.is_goal_node(&node) {
