6735e3db49068745c73f2886ad2927e3d74883fc
Timofey Malko <developer.of.gift@gmail.com>
2023-05-21 21:41:53 -0700

    add traces

--- src/lib.rs
+++ src/lib.rs
@@ -119,11 +119,13 @@ pub fn backward_elim(evalr: Evaluator, n_featurees: usize) -> Node {
     let mut max_eval = evalr.eval_node(&max_node);
     let mut next_node = max_node.clone();
 
+    println!("\nBeginning search.\n");
     println!(
-        "Using no features and “random” evaluation, I get an accuracy of {}%",
+        "Using feature(s) {:?} accuracy is {}%",
+        max_node.get_features(),
         max_eval
     );
-    println!("\nBeginning search.\n");
+    println!("\nFeature set {:?} was best, accuracy is {}%\n", max_node.get_features(), max_eval);
 
     while !next_node.is_empty() {
         let feats = next_node
