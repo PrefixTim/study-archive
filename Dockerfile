02abb44d71e2668d55a838c99caf55cca296c574
Timofei Malko (OverSouls) <developer.of.gift@gmail.com>
2023-10-12 16:41:37 -0700

    refactor

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM rust:latest as build
-LABEL authors="devel"
-WORKDIR /usr/src/sqrt
-COPY . .
-RUN cargo build --release
-CMD ["./target/release/sqrt"]
--- src/mysqrt/mod.rs
+++ src/mysqrt/mod.rs
@@ -6,101 +6,136 @@ const EXP_SHIFT: u32 = f64::MANTISSA_DIGITS - 1;
 const EXP_OFF: i32 = f64::MAX_EXP - 1;
 
 pub trait MySqrt {
-    fn my_sqrt_a(self) -> f64;
-    fn my_sqrt_b(self) -> f64;
-    fn my_sqrt_c(self) -> f64;
-    fn my_sqrt_d(self) -> f64;
+    fn inv_sqrt_approx(self) -> f64;
+    fn halley_sqrt(self) -> f64;
+    fn goldschmidt_sqrt(self) -> f64;
+    fn halley_sqrt_seprt(self) -> f64;
+    fn goldschmidt_sqrt_seprt(self) -> f64;
+    fn halley_sqrt_reg(self) -> f64;
+    fn goldschmidt_sqrt_reg(self) -> f64;
 }
 
-pub fn inv_sqrt_approx(num: f64) -> f64 {
-    f64::from_bits(0x5FE6EB50C7B537A9 - (num.to_bits() >> 1))
+/// Returns a tuple of sqrt of exponent, separate mantissa, and error of sqrt exponent
+///
+fn separate_exp_man(num: f64) -> (f64, f64, f64) {
+    if num - 0. <= f64::EPSILON { return (0., 0., 0.); }
+    let num_bits = num.to_bits();
+    let exp = ((num_bits & EXP_MASK) >> EXP_SHIFT) as i32 - EXP_OFF;
+    let man = num_bits & MAN_MASK | 1f64.to_bits();
+    let sqrt_exp = f64::from_bits((((exp >> 1) + EXP_OFF) as u64) << EXP_SHIFT);
+    (sqrt_exp, f64::from_bits(man), if exp % 2 == 0 { 1f64 } else { SQRT_2 })
 }
 
 impl MySqrt for f64 {
+    /// Returns an approximation of inverse sqrt
+    ///
+    /// # Safety
+    ///
+    /// Calling method on ['num < 0'] is *[undefined behavior]
+    ///
     #[inline]
-    fn my_sqrt_a(self) -> f64 {
-        if self.is_sign_negative() || self.is_nan() || self.is_infinite() { return f64::NAN; }
-        let num_bits = self.to_bits();
-        let exp = ((num_bits & EXP_MASK) >> EXP_SHIFT) as i32 - EXP_OFF;
-        let man = num_bits & MAN_MASK | 1f64.to_bits();
-        let sqrt_exp = f64::from_bits((((exp >> 1) + EXP_OFF) as u64) << EXP_SHIFT);
-        let inv_sqrt_man = f64::from_bits(0x5FE6EB50C7B537A9 - (man >> 1));
-        let manf = f64::from_bits(man);
-
-        let mut sqrt = manf * inv_sqrt_man;
-        let mut h = inv_sqrt_man * 0.5;
-        let mut r;
-        r = sqrt.mul_add(-h, 0.5);
-        sqrt = r.mul_add(sqrt, sqrt);
-        h = r.mul_add(h, h);
-        r = sqrt.mul_add(-h, 0.5);
-        sqrt = r.mul_add(sqrt, sqrt);
-        h = r.mul_add(h, h);
-        r = sqrt.mul_add(-h, 0.5);
-        sqrt = r.mul_add(sqrt, sqrt);
-        h = r.mul_add(h, h);
-        r = sqrt.mul_add(-h, 0.5);
-        sqrt = r.mul_add(sqrt, sqrt);
-
-        sqrt * if exp % 2 == 0 { 1f64 } else { SQRT_2 } * sqrt_exp
+    fn inv_sqrt_approx(self) -> f64 {
+        f64::from_bits(0x5FE6EB50C7B537A9 - (self.to_bits() >> 1))
     }
 
-    #[inline]
-    fn my_sqrt_b(self) -> f64 {
-        if self.is_sign_negative() || self.is_nan() || self.is_infinite() { return f64::NAN; }
-        let num_bits = self.to_bits();
-        let exp = ((num_bits & EXP_MASK) >> EXP_SHIFT) as i32 - EXP_OFF;
-        let man = num_bits & MAN_MASK | 1f64.to_bits();
-        let sqrt_exp = f64::from_bits((((exp >> 1) + EXP_OFF) as u64) << EXP_SHIFT);
-        let mut inv_sqrt_man = f64::from_bits(0x5FE6EB50C7B537A9 - (man >> 1));
-        let manf = f64::from_bits(man);
-
-        let tmp = -manf * inv_sqrt_man * inv_sqrt_man;
-        inv_sqrt_man *= tmp.mul_add(3f64 / 8f64, 10f64 / 8f64).mul_add(tmp, 15f64 / 8f64);
-        let tmp = -manf * inv_sqrt_man * inv_sqrt_man;
-        inv_sqrt_man *= tmp.mul_add(3f64 / 8f64, 10f64 / 8f64).mul_add(tmp, 15f64 / 8f64);
-        let tmp = -manf * inv_sqrt_man * inv_sqrt_man;
-        inv_sqrt_man *= tmp.mul_add(3f64 / 8f64, 10f64 / 8f64).mul_add(tmp, 15f64 / 8f64);
-        let tmp = -manf * inv_sqrt_man * inv_sqrt_man;
-        inv_sqrt_man *= tmp.mul_add(3f64 / 8f64, 10f64 / 8f64).mul_add(tmp, 15f64 / 8f64);
-
-        manf * inv_sqrt_man * if exp % 2 == 0 { 1f64 } else { SQRT_2 } * sqrt_exp
-    }
-
-    #[inline]
-    fn my_sqrt_c(self) -> f64 {
-        let mut inv_sqrt_approx = inv_sqrt_approx(self);
+    /// Returns a sqrt with small error
+    ///
+    /// Returns NaN uf `self` is negative or Inf or Nan
+    ///
+    /// Uses Halley's method for iteration
+    ///
+    fn halley_sqrt(self) -> f64 {
+        if self < 0. || self.is_nan()  { return f64::NAN; }
+        if self.is_infinite() {return f64::INFINITY;}
+        let mut inv_sqrt_approx = self.inv_sqrt_approx();
         let mut tmp;
-        tmp = -self * inv_sqrt_approx * inv_sqrt_approx;
-        inv_sqrt_approx *= tmp.mul_add(3f64 / 8f64, 10f64 / 8f64).mul_add(tmp, 15f64 / 8f64);
-        tmp = -self * inv_sqrt_approx * inv_sqrt_approx;
-        inv_sqrt_approx *= tmp.mul_add(3f64 / 8f64, 10f64 / 8f64).mul_add(tmp, 15f64 / 8f64);
-        tmp = -self * inv_sqrt_approx * inv_sqrt_approx;
-        inv_sqrt_approx *= tmp.mul_add(3f64 / 8f64, 10f64 / 8f64).mul_add(tmp, 15f64 / 8f64);
-        tmp = -self * inv_sqrt_approx * inv_sqrt_approx;
-        inv_sqrt_approx *= tmp.mul_add(3f64 / 8f64, 10f64 / 8f64).mul_add(tmp, 15f64 / 8f64);
-        tmp = -self * inv_sqrt_approx * inv_sqrt_approx;
-        inv_sqrt_approx *= tmp.mul_add(3f64 / 8f64, 10f64 / 8f64).mul_add(tmp, 15f64 / 8f64);
+        for _ in 0..5 {
+            tmp = -self * inv_sqrt_approx * inv_sqrt_approx;
+            inv_sqrt_approx *= tmp.mul_add(3f64 / 8f64, 10f64 / 8f64).mul_add(tmp, 15f64 / 8f64);
+        }
         self * inv_sqrt_approx
     }
 
-    #[inline]
-    fn my_sqrt_d(self) -> f64 {
-        let inv_sqrt_approx = inv_sqrt_approx(self);
-        let mut sqrt = self * inv_sqrt_approx;
-        let mut h = inv_sqrt_approx * 0.5;
-        let mut r;
-        r = sqrt.mul_add(-h, 0.5);
-        sqrt = r.mul_add(sqrt, sqrt);
-        h = r.mul_add(h, h);
-        r = sqrt.mul_add(-h, 0.5);
-        sqrt = r.mul_add(sqrt, sqrt);
-        h = r.mul_add(h, h);
-        r = sqrt.mul_add(-h, 0.5);
-        sqrt = r.mul_add(sqrt, sqrt);
-        h = r.mul_add(h, h);
-        r = sqrt.mul_add(-h, 0.5);
+    /// Returns a sqrt with small error
+    ///
+    /// Returns NaN uf `self` is negative or Inf or Nan
+    ///
+    /// Uses Goldschmidt's method for iteration
+    ///
+    fn goldschmidt_sqrt(self) -> f64 {
+        if self < 0. || self.is_nan()  { return f64::NAN; }
+        if self.is_infinite() {return f64::INFINITY;}
+        let mut sqrt = self * self.inv_sqrt_approx();
+        let mut h = self.inv_sqrt_approx() * 0.5;
+        let mut r = sqrt.mul_add(-h, 0.5);
+        for _ in 0..5 {
+            sqrt = r.mul_add(sqrt, sqrt);
+            h = r.mul_add(h, h);
+            r = sqrt.mul_add(-h, 0.5);
+        }
         r.mul_add(sqrt, sqrt)
+    }
+
+    /// Returns an approximation of sqrt by separating mantissa and exponent
+    /// with Halley's method
+    ///
+    #[inline]
+    fn halley_sqrt_seprt(self) -> f64 {
+        if self < 0. || self.is_nan()  { return f64::NAN; }
+        if self.is_infinite() {return f64::INFINITY;}
+        let (sqrt_exp, manf, correction) = separate_exp_man(self);
+        manf.halley_sqrt() * correction * sqrt_exp
+    }
+
+    /// Returns an approximation of sqrt by separating mantissa and exponent
+    /// with Goldschmidt's method
+    ///
+    #[inline]
+    fn goldschmidt_sqrt_seprt(self) -> f64 {
+        if self < 0. || self.is_nan()  { return f64::NAN; }
+        if self.is_infinite() {return f64::INFINITY;}
+        let (sqrt_exp, manf, correction) = separate_exp_man(self);
+        manf.goldschmidt_sqrt() * correction * sqrt_exp
+    }
+
+    /// Returns a sqrt with small error
+    ///
+    /// Returns NaN uf `self` is negative or Inf or Nan
+    ///
+    /// Uses Halley's method for iteration without mul_add operation
+    ///
+    #[inline]
+    fn halley_sqrt_reg(self) -> f64 {
+        if self < 0. || self.is_nan()  { return f64::NAN; }
+        if self.is_infinite() {return f64::INFINITY;}
+        let mut inv_sqrt_approx = self.inv_sqrt_approx();
+        let mut tmp;
+        for _ in 0..5 {
+            tmp = -self * inv_sqrt_approx * inv_sqrt_approx;
+            inv_sqrt_approx *= (tmp * 3f64 / 8f64 + 10f64 / 8f64) * tmp + 15f64 / 8f64;
+        }
+        self * inv_sqrt_approx
+    }
+
+    /// Returns a sqrt with small error
+    ///
+    /// Returns NaN uf `self` is negative or Inf or Nan
+    ///
+    /// Uses Goldschmidt's method for iteration without mul_add operation
+    ///
+    #[inline]
+    fn goldschmidt_sqrt_reg(self) -> f64 {
+        if self < 0. || self.is_nan()  { return f64::NAN; }
+        if self.is_infinite() {return f64::INFINITY;}
+        let mut sqrt = self * self.inv_sqrt_approx();
+        let mut h = self.inv_sqrt_approx() * 0.5;
+        let mut r = 0.5 - sqrt * h;
+        for _ in 0..5 {
+            sqrt = r * sqrt + sqrt;
+            h = r * h + h;
+            r = 0.5 - sqrt * h;
+        }
+        r * sqrt + sqrt
     }
 }
 
@@ -115,8 +150,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() -> Result<(), Box<dyn std::error::Error>> {
-        let map = (1..=2_000_000).map(|x| (x as f64) / 1024f64).map(|x| x * x);
+    fn draw_sqrt() -> Result<(), Box<dyn std::error::Error>> {
+        let map = (0..=2_000_000).map(|x| (x as f64) / 1024f64).map(|x| x * x);
         let root = BitMapBackend::new("0.png", (640, 480)).into_drawing_area();
         root.fill(&WHITE)?;
         let mut chart = ChartBuilder::on(&root).caption("mysqrt()", ("sans-serif", 50).into_font()).margin(5).x_label_area_size(30).y_label_area_size(30).build_cartesian_2d(1f64..1024f64, 0f64..64f64)?;
@@ -128,39 +163,63 @@ mod tests {
             &BLUE,
         ))?.label("y = x-^.5").legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
         chart.draw_series(LineSeries::new(
-            map.clone().map(|x| (x, x.my_sqrt_c())),
+            map.clone().map(|x| (x, x.goldschmidt_sqrt())),
             &RED,
         ))?.label("y ~= x^-.5").legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
 
         chart.configure_series_labels().background_style(&WHITE.mix(0.8)).border_style(&BLACK).draw()?;
-
         root.present()?;
 
-        let fmap = map.clone().map(|x| i128::abs(x.sqrt().to_bits() as i128 - x.my_sqrt_a().to_bits() as i128)).filter(|&x| x != 0);
-        println!("{}/{}", fmap.clone().count(), map.clone().count());
-        println!("{}", fmap.clone().sum::<i128>());
-        println!("{}", fmap.clone().max().unwrap());
-
-        let fmap = map.clone().map(|x| i128::abs(x.sqrt().to_bits() as i128 - x.my_sqrt_b().to_bits() as i128)).filter(|&x| x != 0);
-        println!("{}/{}", fmap.clone().count(), map.clone().count());
-        println!("{}", fmap.clone().sum::<i128>());
-        println!("{}", fmap.clone().max().unwrap());
-
-        let fmap = map.clone().map(|x| i128::abs(x.sqrt().to_bits() as i128 - x.my_sqrt_c().to_bits() as i128)).filter(|&x| x != 0);
-        println!("{}/{}", fmap.clone().count(), map.clone().count());
-        println!("{}", fmap.clone().sum::<i128>());
-        println!("{}", fmap.clone().max().unwrap());
-
-        let fmap = map.clone().map(|x| i128::abs(x.sqrt().to_bits() as i128 - x.my_sqrt_d().to_bits() as i128)).filter(|&x| x != 0);
-        println!("{}/{}", fmap.clone().count(), map.clone().count());
-        println!("{}", fmap.clone().sum::<i128>());
-        println!("{}", fmap.clone().max().unwrap());
         assert!(true);
         Ok(())
     }
 
+    #[test]
+    fn test_correctnes() {
+        let map = (0..=2_000_000).map(|x| (x as f64) / 1024f64).map(|x| x * x);
+
+        println!("\nhalley_sqrt");
+        let fmap = map.clone().map(|x| i128::abs(x.sqrt().to_bits() as i128 - x.halley_sqrt().to_bits() as i128)).filter(|&x| x != 0);
+        println!("{} different out of {}", fmap.clone().count(), map.clone().count());
+        println!("approx bit wrongs: {}", fmap.clone().sum::<i128>());
+        println!("max difference: {}", fmap.max().unwrap());
+
+        println!("\ngoldschmidt_sqrt");
+        let fmap = map.clone().map(|x| i128::abs(x.sqrt().to_bits() as i128 - x.goldschmidt_sqrt().to_bits() as i128)).filter(|&x| x != 0);
+        println!("{} different out of {}", fmap.clone().count(), map.clone().count());
+        println!("approx bit wrongs: {}", fmap.clone().sum::<i128>());
+        println!("max difference: {}", fmap.max().unwrap());
+
+        println!("\nhalley_sqrt_seprt");
+        let fmap = map.clone().map(|x| i128::abs(x.sqrt().to_bits() as i128 - x.halley_sqrt_seprt().to_bits() as i128)).filter(|&x| x != 0);
+        println!("{} different out of {}", fmap.clone().count(), map.clone().count());
+        println!("approx bit wrongs: {}", fmap.clone().sum::<i128>());
+        println!("max difference: {}", fmap.max().unwrap());
+
+        println!("\ngoldschmidt_sqrt_seprt");
+        let fmap = map.clone().map(|x| i128::abs(x.sqrt().to_bits() as i128 - x.goldschmidt_sqrt_seprt().to_bits() as i128)).filter(|&x| x != 0);
+        println!("{} different out of {}", fmap.clone().count(), map.clone().count());
+        println!("approx bit wrongs: {}", fmap.clone().sum::<i128>());
+        println!("max difference: {}", fmap.max().unwrap());
+
+        println!("\nhalley_sqrt_reg");
+        let fmap = map.clone().map(|x| i128::abs(x.sqrt().to_bits() as i128 - x.halley_sqrt_reg().to_bits() as i128)).filter(|&x| x != 0);
+        println!("{} different out of {}", fmap.clone().count(), map.clone().count());
+        println!("approx bit wrongs: {}", fmap.clone().sum::<i128>());
+        println!("max difference: {}", fmap.max().unwrap());
+
+        println!("\ngoldschmidt_sqrt_reg");
+        let fmap = map.clone().map(|x| i128::abs(x.sqrt().to_bits() as i128 - x.goldschmidt_sqrt_reg().to_bits() as i128)).filter(|&x| x != 0);
+        println!("{} different out of {}", fmap.clone().count(), map.clone().count());
+        println!("approx bit wrongs: {}", fmap.clone().sum::<i128>());
+        println!("max difference: {}", fmap.max().unwrap());
+
+        assert!(true);
+    }
+
+
     fn get_list(f: fn(f64) -> f64) -> f64 {
-        (1..=100_000u64).map(|x| (x as f64) / 1_000f64).map(|x| f(x)).sum()
+        (0..=100_000u64).map(|x| (x as f64) / 1_000f64).map(|x| f(x)).sum()
     }
 
     #[bench]
@@ -169,22 +228,32 @@ mod tests {
     }
 
     #[bench]
-    fn bench_sqrta(b: &mut Bencher) {
-        b.iter(|| get_list(f64::my_sqrt_a));
+    fn bench_halley_sqrt(b: &mut Bencher) {
+        b.iter(|| get_list(f64::halley_sqrt));
     }
 
     #[bench]
-    fn bench_sqrtb(b: &mut Bencher) {
-        b.iter(|| get_list(f64::my_sqrt_b));
+    fn bench_goldschmidt_sqrt(b: &mut Bencher) {
+        b.iter(|| get_list(f64::goldschmidt_sqrt));
     }
 
     #[bench]
-    fn bench_sqrtc(b: &mut Bencher) {
-        b.iter(|| get_list(f64::my_sqrt_c));
+    fn bench_halley_sqrt_seprt(b: &mut Bencher) {
+        b.iter(|| get_list(f64::halley_sqrt_seprt));
     }
 
     #[bench]
-    fn bench_sqrtd(b: &mut Bencher) {
-        b.iter(|| get_list(f64::my_sqrt_d));
+    fn bench_goldschmidt_sqrt_seprt(b: &mut Bencher) {
+        b.iter(|| get_list(f64::goldschmidt_sqrt_seprt));
+    }
+
+    #[bench]
+    fn bench_halley_sqrt_reg(b: &mut Bencher) {
+        b.iter(|| get_list(f64::halley_sqrt_reg));
+    }
+
+    #[bench]
+    fn bench_goldschmidt_sqrt_reg(b: &mut Bencher) {
+        b.iter(|| get_list(f64::goldschmidt_sqrt_reg));
     }
 }
